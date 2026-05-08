a7eb9b7a41277d852a1ceabafe462c67b143cf4e
Santiago <santiago.mj@obmontessori.edu.ph>
2026-05-08 22:01:18 +0800

    added new stuff

--- main.js
+++ main.js
@@ -2,9 +2,13 @@ function loginNow() {
      var user = document.getElementById("usernameEnter").value;
      var pass = document.getElementById("passwordEnter").value;
 
-     if (user === "" || pass === "") {
-          alert("Please fill in both username and password fields.");
-     } else {
-          window.location.href = "notebook.html";
+     // Checks if user has entered a username and passowrd
+     switch (true) {
+          case (user === "" || pass === ""):
+               alert("Please fill in both username and password fields.");
+               break;
+          default:
+               window.location.href = "notebook.html";
+               break;
      }
 }
